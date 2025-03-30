853f0368d47539197623f7a3c2c5dbddd3bfcb90
qn19325 <jrbhall@outlook.com>
2025-03-30 18:41:48 +0100

    add project with eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,jsx,ts,tsx}"],
+    ignores: ["**/{ode_modules}/**/*"],
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: "module",
+
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: ".",
+      },
+    },
+
+    rules: {
+      "no-console": "warn",
+      "no-debugger": "warn",
+      "prefer-const": "warn",
+
+      "@typescript-eslint/no-unused-vars": ["warn", {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      }],
+
+      "@typescript-eslint/no-unnecessary-boolean-literal-compare": "warn",
+      "@typescript-eslint/no-unnecessary-condition": "warn",
+      "@typescript-eslint/no-unnecessary-qualifier": "warn",
+      "@typescript-eslint/no-unnecessary-type-arguments": "warn",
+      "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+      "@typescript-eslint/no-unnecessary-type-constraint": "warn",
+      "@typescript-eslint/no-useless-empty-export": "warn",
+    },
+  }
+]
