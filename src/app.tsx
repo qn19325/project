9bdb76eda3d982895bc67258c46b87ea4c940da0
qn19325 <jrbhall@outlook.com>
2025-04-01 07:01:41 +0100

    rename project and adjust code to be more aligned with project

--- src/app.tsx
+++ src/app.tsx
@@ -2,25 +2,15 @@ import { createSignal } from "solid-js";
 import "./app.css";
 
 export default function App() {
-  const [count, setCount] = createSignal(0);
+    const [capture, setCapture] = createSignal(0);
 
-  return (
-    <main>
-      <h1>Hello world!</h1>
-      <button
-        class="increment"
-        onClick={() => setCount(count() + 1)}
-        type="button"
-      >
-        Clicks: {count()}
-      </button>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
-  );
+    return (
+        <main>
+            <h1>Project</h1>
+            <p>Function will be an application that can scan documents and interpret the content.</p>
+            <button class="increment" onClick={() => setCapture(capture() + 1)} type="button">
+                Capture: {capture()}
+            </button>
+        </main>
+    );
 }
